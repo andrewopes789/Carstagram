f5bd4d55e635d01577ac40aec3784e07ad6d9de1
Andrew Cho <andrewopes789@gmail.com>
2017-11-22 22:58:45 -0800

    Complete user bootstrapping

--- frontend/carstagram.jsx
+++ frontend/carstagram.jsx
@@ -6,8 +6,18 @@ import configureStore from './store/store';
 import { postUser, postSession, deleteSession } from './utils/session_utils';
 
 document.addEventListener("DOMContentLoaded", () => {
-  const store = configureStore();
   const root = document.getElementById("root");
+
+  let preloadedState = undefined;
+  if (window.currentUser) {
+    preloadedState = {
+      session: {
+        currentUser: window.currentUser
+      }
+    };
+  }
+
+  const store = configureStore(preloadedState);
   window.getState = store.getState;
   ReactDOM.render(<Root store={store}/>, root);
 });
--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -8,6 +8,7 @@ const _nullSession = {
 
 export default (state = _nullSession, action) => {
   Object.freeze(state);
+
   switch (action.type) {
     case RECEIVE_CURRENT_USER:
       return Object.assign({}, { currentUser: action.user });
