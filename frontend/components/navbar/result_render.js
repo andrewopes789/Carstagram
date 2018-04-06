fc70284fd098cfed760c3cea5ba6732dce2fecb1
Andrew Cho <andrewopes789@gmail.com>
2018-04-06 15:50:17 -0700

    add link to user profile from search bar

--- frontend/components/navbar/result_render.js
+++ frontend/components/navbar/result_render.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class ResultRender extends React.Component {
   render() {
     let result = this.props.result;
     return (
       <div className='result-item-container'>
-        <img className='result-item-photo' src={result.img_url} />
-        <div>
+        <Link to={`/users/${result.id}`}>
+          <img className='result-item-photo' src={result.img_url} />
+          <div>
           {result.username}
-        </div>
+          </div>
+        </Link>
       </div>
     );
   }
