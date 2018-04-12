42b4a380202423462caf9b1310c5f0d7a76846ed
Andrew Cho <andrewopes789@gmail.com>
2018-04-12 12:39:06 -0700

    add action controller

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,4 @@
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -34,6 +35,7 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
+  config.action_controller.asset_host = ENV['CDN_HOST']
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
